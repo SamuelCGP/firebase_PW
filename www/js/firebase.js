96e7769f70b63ea9b8ad209361d937df070d683c
SamuelCGP <69848773+SamuelCGP@users.noreply.github.com>
2021-11-22 22:53:16 -0300

    added firebase, but not implemented yet

---
+++ www/js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpgUwc4bDRD148DNUCapr9eKcpmwDXolI",
+    authDomain: "fir-pw-f24f8.firebaseapp.com",
+    projectId: "fir-pw-f24f8",
+    storageBucket: "fir-pw-f24f8.appspot.com",
+    messagingSenderId: "715646992330",
+    appId: "1:715646992330:web:4fac8ea87ffc443bfb9933",
+    measurementId: "G-BY4S9P6DEM"
+  };
+
+firebase.initializeApp(firebaseConfig);
---
+++ www/js/funcoes.js
@@ -0,0 +1,77 @@
+function cadastrar(campo, valor) {
+    var db = firebase.firestore();
+
+    db.collection("users").add({
+        campo: valor
+    })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+}
+
+function editar(valor, valorAtual) {
+    var db = firebase.firestore();
+    var ag = db.collection("users").where("first", "==", valor);
+
+    ag.get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                var dados = db.collection("users").doc(doc.id);
+
+                dados.update({
+                    first: valorAtual
+                })
+                    .then(() => {
+                        console.log("Document successfully updated!");
+                    })
+                    .catch((error) => {
+                        // The document probably doesn't exist.
+                        console.error("Error updating document: ", error);
+                    });
+            });
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+}
+
+function listar(valor) {
+    var db = firebase.firestore();
+
+    db.collection("users").where("first", "==", valor)
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+
+                $('#resultado').append(doc.id + ' - ' + doc.data().first + '<br>');
+            });
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+
+}
+
+function excluir(valor) {
+    var db = firebase.firestore();
+
+    db.collection("users").where("first", "==", valor)
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                db.collection("users").doc(doc.id).delete().then(() => {
+                    console.log("Document successfully deleted!");
+                }).catch((error) => {
+                    console.error("Error removing document: ", error);
+                });
+            });
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+}
